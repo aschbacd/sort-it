54755cbcca5ad796ef33c4f4d44825faafc06ae5
Dominik Aschbacher <dominik.aschbacher.00@gmail.com>
2020-04-13 18:00:06 +0200

    Implement concurrency

--- internal/app/sort-it.go
+++ internal/app/sort-it.go
@@ -1,20 +1,14 @@
 package app
 
 import (
-	"crypto/md5"
-	"encoding/hex"
-	"io"
 	"os"
 	"path"
-	"strconv"
 	"strings"
 	"time"
 
 	"github.com/aschbacd/sort-it/internal/utils"
 )
 
-var duplicates map[string]*utils.DuplicateCollection
-var errors []utils.ErrorFile
 var baseParts []string
 
 // Sort uses its supplied parameters to sort files appropriately
@@ -22,164 +16,171 @@ func Sort(sourceFolder, destinationFolder string, copyDuplicates, duplicatesOnly
 	// Check if exiftool is available
 	if !duplicatesOnly {
 		if !utils.CommandAvailable("exiftool") {
-			utils.PrintMessage("exiftool not available", "error", false, false)
+			utils.PrintMessage("exiftool not available", "error")
 			os.Exit(1)
 		}
 	}
 
-	// Get files and file count
-	files, err := utils.GetFilesWithFileCount(sourceFolder)
-	if err != nil {
-		utils.PrintMessage(err.Error(), "error", false, false)
-	}
+	// File lists
+	hashList := []string{}
+	errorList := []utils.File{}
+	sortedList := []utils.File{}
+	duplicatesList := []utils.File{}
 
-	// Initialize fields
-	duplicates = map[string]*utils.DuplicateCollection{}
-	errors = []utils.ErrorFile{}
+	// Channels (get files)
+	hashFiles := make(chan utils.File, 100)
+	errorFiles := make(chan utils.File, 100)
+	fileCount := make(chan int, 100)
 
-	fileCount := float64(len(files))
+	utils.PrintMessage("Getting files ...", "info")
+
+	go func() {
+		if err := utils.GetFilesWithHash(sourceFolder, hashFiles, errorFiles, fileCount); err != nil {
+			utils.PrintMessage(err.Error(), "error")
+			os.Exit(1)
+		}
+	}()
+
 	baseParts = strings.Split(sourceFolder, "/")
 
-	// Sort files
-	for index, filePath := range files {
-		if err := SortFile(filePath, destinationFolder, copyDuplicates, duplicatesOnly, multimediaOnly); err != nil {
-			errors = append(errors, utils.ErrorFile{Path: filePath, Error: err.Error()})
+	// Channels (sort files)
+	sortedFiles := make(chan utils.File, 100)
+	duplicateFiles := make(chan utils.File, 100)
+	errorFilesValid := make(chan utils.File, 100)
+	fileCountValid := make(chan int, 100)
+
+	go func() {
+		for range fileCount {
+			select {
+			case hashFile := <-hashFiles:
+				// Check for duplicate
+				if utils.Contains(hashList, hashFile.Hash) {
+					// Duplicate
+					if copyDuplicates {
+						go CopyDuplicate(hashFile, destinationFolder, duplicateFiles, errorFilesValid)
+					} else {
+						hashFile.RelativePath = strings.Join(strings.Split(hashFile.Path, "/")[len(baseParts):], "/")
+						duplicateFiles <- hashFile
+					}
+				} else {
+					// New file
+					hashList = append(hashList, hashFile.Hash)
+					go SortFile(hashFile, destinationFolder, duplicatesOnly, multimediaOnly, sortedFiles, errorFilesValid)
+				}
+
+				// Increase count
+				fileCountValid <- 0
+			case errorFile := <-errorFiles:
+				// Error
+				errorList = append(errorList, errorFile)
+				utils.PrintMessage(errorFile.Error+" - "+errorFile.Path, "error")
+			}
 		}
 
-		// Increase counter
-		progress := strconv.FormatFloat(float64(index+1)/fileCount*100, 'f', 2, 64)
-		utils.PrintMessage("Progress: "+progress+"%", "info", true, true)
+		// Close channels (get files)
+		close(hashFiles)
+		close(errorFiles)
+		close(fileCountValid)
+	}()
+
+	// Add files to slices
+	for range fileCountValid {
+		select {
+		case hashFileSorted := <-sortedFiles:
+			sortedList = append(sortedList, hashFileSorted)
+			utils.PrintMessage("successfully sorted "+hashFileSorted.Path, "success")
+		case hashFileDuplicate := <-duplicateFiles:
+			duplicatesList = append(duplicatesList, hashFileDuplicate)
+			utils.PrintMessage("successfully sorted "+hashFileDuplicate.Path, "success")
+		case errorFile := <-errorFilesValid:
+			errorList = append(errorList, errorFile)
+			utils.PrintMessage(errorFile.Error+" - "+errorFile.Path, "error")
+		}
 	}
 
-	// Write duplicate and error files
-	duplicatesFolder := sourceFolder
-	if copyDuplicates {
-		duplicatesFolder = path.Join(destinationFolder, "Errors", "Duplicates")
+	// Close channels (sort files)
+	close(sortedFiles)
+	close(duplicateFiles)
+	close(errorFilesValid)
+
+	utils.PrintMessage("Writing duplicate files ...", "info")
+	utils.WriteFileLogs(destinationFolder, sortedList, duplicatesList)
+	utils.PrintMessage("Writing error files ...", "info")
+	utils.WriteErrorFiles(destinationFolder, errorList)
+	utils.PrintMessage("Program finished successfully", "info")
+}
+
+// CopyDuplicate copies a duplicate to its destination folder
+func CopyDuplicate(sourceFile utils.File, destinationFolder string, duplicateFiles chan<- utils.File, errorFiles chan<- utils.File) {
+	// Get relative file path / destination path
+	sourcePathRelative := strings.Join(strings.Split(sourceFile.Path, "/")[len(baseParts):], "/")
+	destinationPath := path.Join(destinationFolder, "Errors", "Duplicates", sourcePathRelative)
+
+	// Copy duplicate
+	err := utils.CopyFile(sourceFile.Path, destinationPath)
+	if err != nil {
+		errorFiles <- utils.File{Path: sourceFile.Path, Error: err.Error()}
+		return
 	}
 
-	println()
-
-	utils.PrintMessage("Writing duplicate files", "info", false, false)
-	utils.WriteDuplicateFiles(destinationFolder, duplicatesFolder, duplicates)
-	utils.PrintMessage("Writing error files", "info", false, false)
-	utils.WriteErrorFiles(destinationFolder, errors)
-	utils.PrintMessage("Program finished successfully", "info", false, false)
+	// Add to channel
+	duplicateFiles <- utils.File{Path: destinationPath, RelativePath: sourcePathRelative, Hash: sourceFile.Hash}
 }
 
 // SortFile sorts a folder
-func SortFile(filePath, destinationFolder string, copyDuplicates, duplicatesOnly, multimediaOnly bool) error {
-	// File -> get md5 hash and check if file is duplicate
-	file, err := os.Open(filePath)
-	defer file.Close()
-	if err != nil {
-		return err
-	}
+func SortFile(sourceFile utils.File, destinationFolder string, duplicatesOnly, multimediaOnly bool, sortedFiles chan<- utils.File, errorFiles chan<- utils.File) {
+	// Get relative file path / destination path
+	sourcePathRelative := strings.Join(strings.Split(sourceFile.Path, "/")[len(baseParts):], "/")
+	destinationPath := path.Join(destinationFolder, "Data", sourcePathRelative)
 
-	// Get md5 hash
-	hash := md5.New()
-	_, err = io.Copy(hash, file)
-	if err != nil {
-		return err
-	}
-
-	// Get md5 sum
-	sum := hex.EncodeToString(hash.Sum(nil))
-
-	fileParts := strings.Split(filePath, "/")
-	filePathRelative := strings.Join(fileParts[len(baseParts):], "/")
-
-	// Check if duplicate
-	if duplicates[sum] != nil {
-		// Copy duplicate
-		if copyDuplicates {
-			destinationPath := path.Join(destinationFolder, "Errors", "Duplicates", filePathRelative)
-
-			err := os.MkdirAll(path.Dir(destinationPath), 0750)
-			if err != nil {
-				return err
-			}
-
-			// Copy duplicate
-			_, err = utils.CopyFile(filePath, destinationPath)
-			if err != nil {
-				return err
-			}
+	if !duplicatesOnly {
+		// Multimedia
+		metaFile, err := utils.GetFileMetadata(sourceFile.Path)
+		if err != nil && multimediaOnly {
+			errorFiles <- utils.File{Path: sourceFile.Path, Error: err.Error()}
+			return
 		}
 
-		// Save duplicate
-		duplicatesList := duplicates[sum].Duplicates
-		duplicatesList = append(duplicatesList, filePathRelative)
+		// Check if create date available
+		fileDate, err := time.Parse("2006:01:02 15:04:05", metaFile.CreateDate)
+		if err != nil {
+			// Invalid creation date / time
+			fileDate, _ = time.Parse("2006:01:02 15:04:05-07:00", metaFile.CreateDate)
+		}
 
-		duplicates[sum] = &utils.DuplicateCollection{Path: duplicates[sum].Path, Hash: sum, Duplicates: duplicatesList}
-	} else {
-		// New file
-		destinationPath := path.Join(destinationFolder, "Data", filePathRelative)
-		valid := true
+		mimeTypeParts := strings.Split(metaFile.MIMEType, "/")
 
-		if !duplicatesOnly {
-			// Multimedia
-			metaFile, err := utils.GetFileMetadata(filePath)
-			if err != nil {
-				return err
-			}
-
-			// Check if create date available
-			fileDate, err := time.Parse("2006:01:02 15:04:05", metaFile.CreateDate)
-			if err != nil {
-				// Invalid creation date / time
-				fileDate, _ = time.Parse("2006:01:02 15:04:05-07:00", metaFile.CreateDate)
-			}
-
-			mimeTypeParts := strings.Split(metaFile.MIMEType, "/")
-
-			if !fileDate.IsZero() || mimeTypeParts[0] == "audio" {
-				switch mimeTypeParts[0] {
-				case "image":
-					// Image
-					destinationPath = path.Join(destinationFolder, "Multimedia", "Pictures", fileDate.Format("2006"), fileDate.Format("01 January"), fileDate.Format("IMG_20060102_150405.")+metaFile.FileTypeExtension)
-				case "video":
-					// Video
-					destinationPath = path.Join(destinationFolder, "Multimedia", "Videos", fileDate.Format("2006"), fileDate.Format("01 January"), fileDate.Format("VID_20060102_150405.")+metaFile.FileTypeExtension)
-				case "audio":
-					// Audio
-					if metaFile.Artist != "" && metaFile.Album != "" && metaFile.Title != "" {
-						destinationPath = path.Join(destinationFolder, "Multimedia", "Audio", "Music", metaFile.Artist, metaFile.Album, metaFile.Title+"."+metaFile.FileTypeExtension)
-					} else {
-						destinationPath = path.Join(destinationFolder, "Multimedia", "Audio", "Sounds", fileDate.Format("2006"), fileDate.Format("01 January"), fileDate.Format("AUD_20060102_150405.")+metaFile.FileTypeExtension)
-					}
-				default:
-					// Only copy multimedia files
-					if multimediaOnly {
-						valid = false
-					}
-				}
-			} else {
-				// Only copy multimedia files
-				if multimediaOnly {
-					valid = false
+		if !fileDate.IsZero() || mimeTypeParts[0] == "audio" {
+			switch mimeTypeParts[0] {
+			case "image":
+				// Image
+				destinationPath = path.Join(destinationFolder, "Multimedia", "Pictures", fileDate.Format("2006"), fileDate.Format("01 January"), fileDate.Format("IMG_20060102_150405.")+metaFile.FileTypeExtension)
+			case "video":
+				// Video
+				destinationPath = path.Join(destinationFolder, "Multimedia", "Videos", fileDate.Format("2006"), fileDate.Format("01 January"), fileDate.Format("VID_20060102_150405.")+metaFile.FileTypeExtension)
+			case "audio":
+				// Audio
+				if metaFile.Artist != "" && metaFile.Album != "" && metaFile.Title != "" {
+					destinationPath = path.Join(destinationFolder, "Multimedia", "Audio", "Music", metaFile.Artist, metaFile.Album, metaFile.Title+"."+metaFile.FileTypeExtension)
+				} else {
+					destinationPath = path.Join(destinationFolder, "Multimedia", "Audio", "Sounds", fileDate.Format("2006"), fileDate.Format("01 January"), fileDate.Format("AUD_20060102_150405.")+metaFile.FileTypeExtension)
 				}
 			}
 		}
-
-		if valid {
-			// Copy file
-			err = os.MkdirAll(path.Dir(destinationPath), 0750)
-			if err != nil {
-				return err
-			}
-
-			_, err = utils.CopyFile(filePath, destinationPath)
-			if err != nil {
-				return err
-			}
-
-			destinationFolderParts := strings.Split(destinationFolder, "/")
-			destinationFileParts := strings.Split(destinationPath, "/")
-			destinationFilePathRelative := strings.Join(destinationFileParts[len(destinationFolderParts):], "/")
-
-			duplicates[sum] = &utils.DuplicateCollection{Path: destinationFilePathRelative, Hash: sum}
-		}
 	}
 
-	return nil
+	if destinationPath != path.Join(destinationFolder, "Data", sourcePathRelative) || !multimediaOnly {
+		// Copy file
+		err := utils.CopyFile(sourceFile.Path, destinationPath)
+		if err != nil {
+			errorFiles <- utils.File{Path: sourceFile.Path, Error: err.Error()}
+			return
+		}
+
+		// Get relative destination path
+		destinationPathRelative := strings.Join(strings.Split(destinationPath, "/")[len(strings.Split(destinationFolder, "/")):], "/")
+
+		// Add to channel
+		sortedFiles <- utils.File{Path: destinationPath, RelativePath: destinationPathRelative, Hash: sourceFile.Hash}
+	}
 }
